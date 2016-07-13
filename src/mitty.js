0cdb70143408a0ada59617fb2f2475bf602a3021
Damir Brekalo <dbrekalo@gmail.com>
2016-07-13 09:09:17 +0200

    - work on test suite

--- src/mitty.js
+++ src/mitty.js
@@ -1,5 +1,6 @@
 (function(root, factory) {
 
+    /* istanbul ignore next */
     if (typeof define === 'function' && define.amd) {
         define([], factory);
     } else if (typeof module === 'object' && module.exports) {
---
+++ test/index.js
@@ -0,0 +1,140 @@
+var assert = require("chai").assert;
+var mitty = require("../");
+
+describe("mitty", function() {
+
+    it('augments object with events api', function() {
+
+        var musician = mitty({name: 'George'});
+
+        assert.isFunction(musician.on);
+        assert.isFunction(musician.off);
+        assert.isFunction(musician.trigger);
+        assert.isFunction(musician.listenTo);
+        assert.isFunction(musician.stopListening);
+
+    });
+
+    it('returns same object to enable method chaining', function() {
+
+        var musician = {name: 'George'};
+        var mittyMusician = mitty(musician);
+
+        assert.strictEqual(musician,
+            mittyMusician
+                .on('play', function(){})
+                .off('play')
+                .trigger('play')
+                .listenTo({}, 'play')
+                .stopListening({})
+        );
+
+    });
+
+    it('correctly plays "on / trigger" scenario', function() {
+
+        var musician = mitty({name: 'George'});
+
+        musician.on('play', function() { musician.isPlaying = true; }).trigger('play');
+
+        assert.isTrue(musician.isPlaying);
+
+    });
+
+    it('correctly plays "on / trigger" scenario when passing event data', function() {
+
+        var musician = mitty({name: 'George'});
+        var instrument = 'drums';
+
+        musician.on('play', function(instrument) {
+            musician.isPlayingInstrument = instrument;
+        }).trigger('play', instrument);
+
+        assert.equal(musician.isPlayingInstrument, instrument);
+
+    });
+
+    it('on registers multiple callbacks for same event', function() {
+
+        var musician = mitty({name: 'George'});
+        var counter = 0;
+
+        musician.on('play', function() { ++counter; });
+        musician.on('play', function() { counter += 2; });
+
+        musician.trigger('play');
+
+        assert.equal(counter, 3);
+
+    });
+
+    it('listenTo enables listening to other object events', function() {
+
+        var musician = mitty({name: 'George'});
+        var instrument = mitty({type: 'Guitar'});
+
+        instrument.listenTo(musician, 'play', function() {
+            instrument.isProducingSound = true;
+        });
+
+        musician.trigger('play');
+
+        assert.isTrue(instrument.isProducingSound);
+
+    });
+
+    it('off removes only specified callback', function() {
+
+        var musician = mitty({name: 'George'});
+        var instrument = 'drums';
+        var playHandler = function(instrument) {
+            musician.isPlayingInstrument = instrument;
+        };
+        var secondPlayHandler = function() {
+            musician.isPlayingAgain = true;
+        };
+
+        musician.on('play', playHandler).on('play', secondPlayHandler);
+
+        musician.off('play', playHandler);
+
+        musician.trigger('play', instrument);
+
+        assert.isUndefined(musician.isPlayingInstrument);
+        assert.isTrue(musician.isPlayingAgain);
+
+    });
+
+    it('off removes all callbacks for event name', function() {
+
+        var musician = mitty({name: 'George'});
+        var playCounter = 0;
+        var isDancing = false;
+
+        musician.on('play', function() { ++playCounter }).on('play', function() { ++playCounter });
+        musician.on('dance', function() { isDancing = true });
+
+        musician.off('play').trigger('play').trigger('dance');
+
+        assert.equal(playCounter, 0);
+        assert.isTrue(isDancing);
+
+    });
+
+    it('off removes all listeners when called with no arguments', function() {
+
+        var musician = mitty({name: 'George'});
+        var playCounter = 0;
+        var isDancing = false;
+
+        musician.on('play', function() { ++playCounter }).on('play', function() { ++playCounter });
+        musician.on('dance', function() { isDancing = true });
+
+        musician.off().trigger('play').trigger('dance');
+
+        assert.equal(playCounter, 0);
+        assert.isFalse(isDancing);
+
+    });
+
+});
